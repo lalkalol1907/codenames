53601816c28aca253e01e4883a093f532840de06
lalkalol1907 <kostyaleonov13@yandex.ru>
2026-06-10 01:27:22 +0300

    fix

--- src/main/kotlin/com/lalkalol/CodenamesApplication.kt
+++ src/main/kotlin/com/lalkalol/CodenamesApplication.kt
@@ -4,9 +4,11 @@ import com.lalkalol.config.AppProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties::class)
+@EnableJpaRepositories(basePackages = ["com.lalkalol.db.jpa"])
 class CodenamesApplication
 
 fun main(args: Array<String>) {
